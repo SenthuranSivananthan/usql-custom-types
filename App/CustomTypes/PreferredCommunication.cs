2ddfa238cdecdc0959903e3fffd0ab55f3630261
Senthuran Sivananthan [MSFT] <sesivan@microsoft.com>
2017-02-07 06:22:42 -0800

    Initial Commit

---
+++ App/CustomTypes/PreferredCommunication.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+
+namespace CustomTypes
+{
+    [SqlUserDefinedType(typeof(PreferredCommunicationFormatter))]
+
+    public class PreferredCommunication
+    {
+        public SqlArray<string> Channels { get; set; }
+
+        public static PreferredCommunication ParseFrom(string input, char delimiter)
+        {
+            return new PreferredCommunication { Channels = new SqlArray<string>(input.Split(delimiter)) };
+        }
+    }
+}
---
+++ App/CustomTypes/PreferredCommunicationFormatter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System.IO;
+using System.Text;
+
+namespace CustomTypes
+{
+    public class PreferredCommunicationFormatter : IFormatter<PreferredCommunication>
+    {
+        private readonly char DELIMITER = '|';
+
+        public PreferredCommunication Deserialize(IColumnReader reader, ISerializationContext context)
+        {
+            var preferredComm = new PreferredCommunication();
+            using (var binaryReader = new BinaryReader(reader.BaseStream))
+            {
+                preferredComm.Channels = new SqlArray<string>(binaryReader.ReadString().Split(DELIMITER));
+            }
+
+            return preferredComm;
+        }
+
+        public void Serialize(PreferredCommunication instance, IColumnWriter writer, ISerializationContext context)
+        {
+            using (var binaryWriter = new BinaryWriter(writer.BaseStream))
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var channel in instance.Channels)
+                {
+                    if (sb.Length > 0)
+                    {
+                        sb.Append(DELIMITER);
+                    }
+
+                    sb.Append(channel);
+                }
+
+                binaryWriter.Write(sb.ToString());
+                binaryWriter.Flush();
+            }
+        }
+    }
+}
